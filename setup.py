e166f349f35d15d1fc3663875b341bd653a01d6b
nalourie <nalourie@gmail.com>
2014-09-08 17:45:44 -0400

    forgot to add manifest, etc...

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name='charitycheck',
+    version='1.0',
+    author='Nicholas A. Lourie',
+    author_email='developer.nick@kozbox.com',
+    packages=['charitycheck',],
+    license='MIT License',
+    description=('a small module to verify information'
+                 'about nonprofits using their EINs'),
+    long_description=open('README.md').read(),
+    keywords="nonprofit nonprofits EIN IRS verify",
+    url="https://github.com/nalourie/charitycheck",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
+    
